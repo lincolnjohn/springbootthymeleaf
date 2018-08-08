4697044325975c2c475c37defc9ee5f5dbf1f0e6
Lincoln John <lincolnjohn@gmail.com>
2018-08-07 23:10:47 -0300

    Finalizando a camada de Srvico

--- demo-mvc/src/main/java/com/thot/curso/boot/service/DepartamentoService.java
+++ demo-mvc/src/main/java/com/thot/curso/boot/service/DepartamentoService.java
@@ -1,5 +1,19 @@
 package com.thot.curso.boot.service;
 
+import java.util.List;
+
+import com.thot.curso.boot.domain.Departamento;
+
 public interface DepartamentoService {
+	
+void salvar(Departamento departamento);
+	
+	void editar(Departamento departamento);
+	
+	void excluir(Long id);
+	
+	Departamento buscarPorId(Long id);
+	
+	List<Departamento> buscarTodos();
 
 }
--- demo-mvc/src/main/java/com/thot/curso/boot/service/FuncionarioService.java
+++ demo-mvc/src/main/java/com/thot/curso/boot/service/FuncionarioService.java
@@ -1,5 +1,18 @@
 package com.thot.curso.boot.service;
 
+import java.util.List;
+
+import com.thot.curso.boot.domain.Funcionario;
+
 public interface FuncionarioService {
 
+void salvar(Funcionario funcionario);
+	
+	void editar(Funcionario funcionario);
+	
+	void excluir(Long id);
+	
+	Funcionario buscarPorId(Long id);
+	
+	List<Funcionario> buscarTodos();
 }
