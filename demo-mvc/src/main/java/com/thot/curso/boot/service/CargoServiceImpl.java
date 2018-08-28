db9be8772bbdc30939fd80863a6f4f980f23e97d
Lincoln John <lincolnjohn@gmail.com>
2018-08-28 15:02:13 -0300

    CRUD Cargo

--- demo-mvc/src/main/java/com/thot/curso/boot/service/CargoServiceImpl.java
+++ demo-mvc/src/main/java/com/thot/curso/boot/service/CargoServiceImpl.java
@@ -47,5 +47,13 @@ public class CargoServiceImpl implements CargoService{
 	public List<Cargo> buscarTodos() {
 		return dao.findAll();
 	}
+	
+	@Override
+	public boolean cargoTemFuncionarios(Long id) {
+		if(buscarPorId(id).getFuncionarios().isEmpty()) {
+			return false;
+		}
+		return true;
+	}
 
 }
